d7c8df2517a8169d868f7d6c122b10998da773aa
Jesvi Jonathan <jesvi22j@gmail.com>
2022-04-26 00:54:35 +0530

    side panel and many more
    Signed-off-by: Jesvi Jonathan <jesvi22j@gmail.com>

--- lanimation.js
+++ lanimation.js
@@ -1,4 +1,4 @@
-setTimeout(function () {
-  document.querySelector(".svg1").style.display = "none";
-  document.querySelector(".svg2").style.display = "none";
-}, 2500);
+// setTimeout(function () {
+//   document.querySelector(".svg1").style.display = "none";
+//   document.querySelector(".svg2").style.display = "none";
+// }, 2500);
