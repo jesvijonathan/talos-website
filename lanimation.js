7b354f46b346029b271e75b669f68f933657d6f8
Jesvi Jonathan <jesvi22j@gmail.com>
2022-05-04 18:07:04 +0530

    optimization #1
    Signed-off-by: Jesvi Jonathan <jesvi22j@gmail.com>

--- lanimation.js
+++
@@ -1,4 +0,0 @@
-// setTimeout(function () {
-//   document.querySelector(".svg1").style.display = "none";
-//   document.querySelector(".svg2").style.display = "none";
-// }, 2500);
--- nig.js
+++
@@ -1,2 +0,0 @@
-//data for forms/popup
-
